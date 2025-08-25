6c99ef8ca0032ba4c2a4da4223ac095282d9f413
Cristian Peña <cristiandpm.2004@gmail.com>
2025-08-25 09:41:33 -0500

    initia commint

---
+++ src/main/java/com/procol/procolombia/ProColombiaApplication.java
@@ -0,0 +1,13 @@
+package com.procol.procolombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProColombiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProColombiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/procol/procolombia/ProColombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procol.procolombia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProColombiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
