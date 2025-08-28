a1f7fe91337464d9c5b407905c237bcc0d01dc37
Cristian Peña <cristiandpm.2004@gmail.com>
2025-08-27 21:09:20 -0500

    feat (repositories): Se termino el repositorio de RelVacantePalabraClave

--- src/main/java/com/procol/procolombia/repositories/RelVacantePalabraclaveRepository.java
+++ src/main/java/com/procol/procolombia/repositories/RelVacantePalabraclaveRepository.java
@@ -4,5 +4,6 @@ import com.procol.procolombia.entities.RelVacantePalabraclave;
 import com.procol.procolombia.entities.RelVacantePalabraclaveId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface RelVacantePalabraclaveRepository extends JpaRepository<RelVacantePalabraclave, RelVacantePalabraclaveId> {
 }
