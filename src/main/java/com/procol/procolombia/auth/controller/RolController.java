fa536c7250bbfb47fbdf75e0022e58735f7fa2e9
CJPRO10 <88499162+CJPRO10@users.noreply.github.com>
2025-09-11 22:53:46 -0500

    RolController creado

--- src/main/java/com/procol/procolombia/auth/controller/RolController.java
+++ src/main/java/com/procol/procolombia/auth/controller/RolController.java
@@ -1,4 +1,29 @@
 package com.procol.procolombia.auth.controller;
 
+import com.procol.procolombia.auth.dto.Response.ApiResponseDTO;
+import com.procol.procolombia.auth.dto.Response.RolResponseDTO;
+import com.procol.procolombia.auth.service.RolService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/roles")
 public class RolController {
+
+    private final RolService rolService;
+
+    public RolController(RolService rolService) {
+        this.rolService = rolService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponseDTO<List<RolResponseDTO>>> listarRols() {
+        ApiResponseDTO<List<RolResponseDTO>> roles = rolService.listarRols();
+        return ResponseEntity.status(listarRols().getStatusCode()).body(roles);
+    }
 }
