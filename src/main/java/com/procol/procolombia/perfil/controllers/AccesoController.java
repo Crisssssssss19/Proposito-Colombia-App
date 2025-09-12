39e9a902e7a15918e79c78707937b046c59768e6
RAAR-18 <raacuna@unimagdalena.edu.co>
2025-09-12 13:14:11 -0500

    añadir validación

--- src/main/java/com/procol/procolombia/perfil/controllers/AccesoController.java
+++ src/main/java/com/procol/procolombia/perfil/controllers/AccesoController.java
@@ -3,6 +3,7 @@ package com.procol.procolombia.perfil.controllers;
 import com.procol.procolombia.perfil.dtos.request.SaveAcceso;
 import com.procol.procolombia.perfil.dtos.response.GetAcceso;
 import com.procol.procolombia.perfil.services.AccesoService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +17,9 @@ public class AccesoController {
     }
 
     @PostMapping
-    public ResponseEntity<GetAcceso> crearAcceso(@RequestBody SaveAcceso saveAcceso){
-        return ResponseEntity.ok(accesoService.crearAcceso(saveAcceso));
+    public ResponseEntity<GetAcceso> crearAcceso(@Valid @RequestBody SaveAcceso saveAcceso){
+        GetAcceso acceso = accesoService.crearAcceso(saveAcceso);
+        return ResponseEntity.status(201).body(acceso);
     }
 
     @PutMapping("/{id}")
