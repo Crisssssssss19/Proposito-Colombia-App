69c24da801f8f9c6427ccd393531d282b14be9a6
RAAR-18 <raacuna@unimagdalena.edu.co>
2025-09-11 23:18:53 -0500

    Implementar UsuarioController

---
+++ src/main/java/com/procol/procolombia/perfil/controllers/UsuarioController.java
@@ -0,0 +1,45 @@
+package com.procol.procolombia.perfil.controllers;
+
+import com.procol.procolombia.perfil.dtos.request.SaveUsuario;
+import com.procol.procolombia.perfil.dtos.response.GetUsuario;
+import com.procol.procolombia.perfil.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<GetUsuario> crearUsuario(@RequestBody SaveUsuario saveUsuario) {
+        return ResponseEntity.ok(usuarioService.crearUsuario(saveUsuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GetUsuario> actualizarUsuario(@PathVariable Integer id, @RequestBody SaveUsuario saveUsuario) {
+        return ResponseEntity.ok(usuarioService.actualizarUsuario(id, saveUsuario));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetUsuario> getUsuario(@PathVariable Integer id) {
+        return ResponseEntity.ok(usuarioService.obtenerUsuarioPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetUsuario>> listarUsuarios() {
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarUsuario(@PathVariable Integer id) {
+        usuarioService.eliminarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
+}
