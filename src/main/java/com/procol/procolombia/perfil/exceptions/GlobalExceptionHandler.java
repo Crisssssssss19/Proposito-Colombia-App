dd3c9a790e4069d6b3fbc4ed8dd9c629c12dfbf2
RAAR-18 <raacuna@unimagdalena.edu.co>
2025-09-12 17:00:26 -0500

    implementar excepciones

--- src/main/java/com/procol/procolombia/perfil/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/procol/procolombia/perfil/exceptions/GlobalExceptionHandler.java
@@ -1,24 +1,39 @@
 package com.procol.procolombia.perfil.exceptions;
 
 import com.procol.procolombia.perfil.dtos.response.ApiResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        ApiResponse<Void> response = ApiResponse.error(errorMessage, null, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiResponse<Void>> handleRuntimeException(RuntimeException ex) {
-        ApiResponse<Void> response = new ApiResponse<>("error", ex.getMessage(), null);
+        ApiResponse<Void> response = ApiResponse.error(ex.getMessage(),null, HttpStatus.BAD_REQUEST);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleEntityNotFoundException(EntityNotFoundException ex) {
+        ApiResponse<Void> response = ApiResponse.error(ex.getMessage(), null, HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
         ex.printStackTrace();
-        ApiResponse<Void> response = new ApiResponse<>("error", "Ha ocurrido un error inesperado", null);
+        ApiResponse<Void> response = ApiResponse.error("Ha ocurrido un error inesperado", null, HttpStatus.INTERNAL_SERVER_ERROR);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
