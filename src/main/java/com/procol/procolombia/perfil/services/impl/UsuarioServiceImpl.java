49ae3b586ed0e2ebcec0b26cc8644e441cca8090
RAAR-18 <raacuna@unimagdalena.edu.co>
2025-10-01 17:42:52 -0500

    feat(UsuarioServiceImpl): add lógica para asignar habilidades y competencias

--- src/main/java/com/procol/procolombia/perfil/services/impl/UsuarioServiceImpl.java
+++ src/main/java/com/procol/procolombia/perfil/services/impl/UsuarioServiceImpl.java
@@ -6,23 +6,30 @@ import com.procol.procolombia.auth.repositories.UbicacioneRepository;
 import com.procol.procolombia.auth.repositories.UsuarioRepository;
 import com.procol.procolombia.perfil.dtos.request.SaveUsuario;
 import com.procol.procolombia.perfil.dtos.response.GetUsuario;
+import com.procol.procolombia.perfil.entities.Talento;
 import com.procol.procolombia.perfil.mappers.UsuarioMapper;
+import com.procol.procolombia.perfil.repositories.TalentoRepository;
 import com.procol.procolombia.perfil.services.UsuarioService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Transactional
 public class UsuarioServiceImpl implements UsuarioService {
     private UsuarioRepository usuarioRepository;
     private UsuarioMapper usuarioMapper;
     private UbicacioneRepository ubicacioneRepository;
+    private TalentoRepository talentoRepository;
 
-    public UsuarioServiceImpl(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper, UbicacioneRepository ubicacioneRepository) {
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper, UbicacioneRepository ubicacioneRepository, TalentoRepository talentoRepository) {
         this.usuarioRepository = usuarioRepository;
         this.usuarioMapper = usuarioMapper;
         this.ubicacioneRepository = ubicacioneRepository;
+        this.talentoRepository = talentoRepository;
     }
 
     @Override
@@ -40,11 +47,34 @@ public class UsuarioServiceImpl implements UsuarioService {
         Usuario usuario = usuarioRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
 
-        usuario.setTipoDocumentoUsuario(saveUsuario.tipoDocumento());
-        usuario.setEstadoUsuario(saveUsuario.estado());
         if (saveUsuario.idUbicacion() != null) {
             usuario.setIdUbicacion(obtenerUbicacionPorId(saveUsuario.idUbicacion()));
         }
+
+        usuario.getTalentos().clear();
+
+        if(saveUsuario.habilidades() != null && !saveUsuario.habilidades().isBlank()) {
+            List<Talento> habilidades = Arrays.stream(saveUsuario.habilidades().split(","))
+                    .map(String::trim)
+                    .map(nombre -> obtenerOrCrearTalento(nombre, (short) 1))
+                    .toList();
+            usuario.getTalentos().addAll(habilidades);
+        }
+
+        if(saveUsuario.competencias() != null && !saveUsuario.competencias().isBlank()) {
+            List<Talento> competencias = Arrays.stream(saveUsuario.competencias().split(","))
+                    .map(String::trim)
+                    .map(nombre -> obtenerOrCrearTalento(nombre, (short) 2))
+                    .toList();
+            usuario.getTalentos().addAll(competencias);
+        }
+
+        usuario.setNombresUsuario(saveUsuario.nombres());
+        usuario.setApellidosUsuario(saveUsuario.apellidos());
+        usuario.setTipoDocumentoUsuario(saveUsuario.tipoDocumento());
+        usuario.setEstadoUsuario(saveUsuario.estado());
+        usuario.setDocumentoUsuario(saveUsuario.documento());
+
         Usuario usuarioActualizado = usuarioRepository.save(usuario);
         return usuarioMapper.usuarioToGetUsuario(usuarioActualizado);
     }
@@ -72,5 +102,15 @@ public class UsuarioServiceImpl implements UsuarioService {
     private Ubicacione obtenerUbicacionPorId(Integer id) {
         return ubicacioneRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Ubicación no encontrada"));
+    }
+
+    private Talento obtenerOrCrearTalento(String nombre, Short tipo) {
+        return talentoRepository.findByNombreAndTipo(nombre, tipo)
+                .orElseGet(() -> {
+                    Talento nuevoTalento = new Talento();
+                    nuevoTalento.setNombre(nombre);
+                    nuevoTalento.setTipo(tipo);
+                    return talentoRepository.save(nuevoTalento);
+                });
     }
 }
