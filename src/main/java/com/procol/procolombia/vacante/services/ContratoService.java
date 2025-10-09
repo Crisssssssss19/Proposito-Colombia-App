267e98a29b2a7d59412928f32698ca5b014b295d
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-10-09 14:38:05 -0500

    definiciones de metodos necesarios

--- src/main/java/com/procol/procolombia/vacante/services/ContratoService.java
+++ src/main/java/com/procol/procolombia/vacante/services/ContratoService.java
@@ -1,4 +1,10 @@
 package com.procol.procolombia.vacante.services;
 
+import com.procol.procolombia.vacante.dto.ContratoDto;
+
+import java.util.List;
+
 public interface ContratoService {
+    ContratoDto buscarContratoPorId(int id);
+    List<ContratoDto> listarContratos();
 }
